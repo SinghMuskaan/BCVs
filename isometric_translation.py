c9b8f758582f1ea25cdb79b5dce8b8d2d1cf25fd
Nid989 <75028682+Nid989@users.noreply.github.com>
2022-05-04 20:42:12 +0530

    Update isometric_translation.py

--- isometric_translation.py
+++ isometric_translation.py
@@ -1,16 +1,28 @@
+import os
 import boto3
+import shutil
+# from tqdm.notebook import tqdm
 from tqdm import tqdm
+import pandas as pd
+from torch.utils.data import DataLoader
+from datasets import load_dataset
+import transformers
 import subprocess
-from transformers import MarianMTModel, MarianTokenizer
+from transformers import MarianMTModel, MarianTokenizer, AutoTokenizer, AutoModel, MBartForConditionalGeneration
+
+tqdm.pandas()
 
 # utilities
-  
 def read_minute(path_to_file):
   with open(path_to_file, 'r') as f:
     minute = [line.strip() for line in f]  
   return minute
 
-def translate_minutes(minute: list):
+def translate_minutes(
+    minute: list, 
+    tokenizer: transformers.models.marian.tokenization_marian.MarianTokenizer, 
+    model: transformers.models.marian.modeling_marian.MarianMTModel
+):
   translated_minutes = []
   for source_sentence in tqdm(minute, total=len(minute)):
     translated = model.generate(**tokenizer(source_sentence, return_tensors="pt", padding=True))  
@@ -22,15 +34,123 @@ def save_2_text(translated_minutes: list, path_to_file: str):
     for listitem in translated_minutes:
         filehandle.write('%s\n' % listitem)
 
-model_checkpoints = "enimai/OPUS-mt-en-fr-finetuned-MUST-C"
-tokenizer = MarianTokenizer.from_pretrained(model_checkpoints)
-model = MarianMTModel.from_pretrained(model_checkpoints)
+def check_for_verbosity(input_text, target_text):
+  if not input_text or not target_text:
+      return False
+  ts_ratio = len(target_text)/len(input_text)
+  if not (ts_ratio >= 0.90 and ts_ratio <= 1.10):
+    return True
+  return False
 
-def generate_translated_document(process_code):
-      
-  # translate English minutes to French
-  minute = read_minute(f"output/meeting-minutes/{process_code}.txt")
-  translated_minutes = translate_minutes(minute)
+def append_paraphrase_prompt(input_text, target_text):
+  ts_ratio = len(target_text)/len(input_text)
+  prefix = None
+  if ts_ratio < 0.90:
+    prefix = "paraphrase long"
+  elif ts_ratio > 1.10:
+    prefix = "paraphrase short"
+  target_text = prefix + " " + target_text
+  return target_text
 
-  path_to_file = f"output/meeting-minutes-translated/translated_fr_{process_code}.txt"
-  save_2_text(translated_minutes, path_to_file)
+# -------------------------------------------------------- #
+
+language_2_mt_model_mappings = {
+    'de': 'enimai/opus-mt-en-de-finetuned-en-to-de',
+    'fr': 'enimai/OPUS-mt-en-fr-finetuned-MUST-C',
+    'ru': 'enimai/opus-mt-en-ru-finetuned-en-to-ru', 
+    'it': 'enimai/opus-mt-en-it-finetuned-en-to-it',
+    'hi': 'enimai/opus-mt-en-hi-finetuned-en-to-hi',
+}
+language_2_para_model_mappings = {
+    'de': 'enimai/mbart-large-50-paraphrase-finetuned-for-de',
+    'fr': 'enimai/mbart-large-50-paraphrase-finetuned-for-fr',
+    'ru': 'enimai/mbart-large-50-paraphrase-finetuned-for-ru'
+}
+
+def process_translation(languages, process_code):
+    current_directory = os.getcwd()
+    path_to_output_folder = os.path.join(current_directory, "output")
+    path_to_minutes_folder = os.path.join(path_to_output_folder, 'meeting-minutes')
+    path_to_translated_minutes_folder = os.path.join(path_to_output_folder, 'meeting-minutes-translated')
+    path_to_minute_file = [os.path.join(path_to_minutes_folder, file) for file in os.listdir(path_to_minutes_folder) if file == f"{process_code}.txt"][0]    
+    for language in languages:
+        minutes = read_minute(path_to_minute_file)
+        model_name = language_2_mt_model_mappings[language]
+        tokenizer = MarianTokenizer.from_pretrained(model_name)
+        model = MarianMTModel.from_pretrained(model_name)
+        translated = translate_minutes(minutes, tokenizer, model)
+        path_to_translated_file = os.path.join(path_to_translated_minutes_folder, f"translated_{language}_{process_code}.txt")
+        save_2_text(translated, path_to_translated_file)
+
+def process_paraphrase(languages, process_code):
+    para_languages = list(language_2_para_model_mappings.keys())
+    for language in languages:
+        if language not in para_languages:
+            continue
+
+        current_directory = os.getcwd()
+        path_to_output_folder = os.path.join(current_directory, "output")
+        path_to_minutes_folder = os.path.join(path_to_output_folder, 'meeting-minutes')
+        path_to_translated_minutes_folder = os.path.join(path_to_output_folder, 'meeting-minutes-translated')
+        path_to_minute_file = [os.path.join(path_to_minutes_folder, file) for file in os.listdir(path_to_minutes_folder) if file == f"{process_code}.txt"][0]    
+        path_to_translated_file = [os.path.join(path_to_translated_minutes_folder, file) for file in os.listdir(path_to_translated_minutes_folder) if file == f"translated_{language}_{process_code}.txt"][0]
+        minutes = read_minute(path_to_minute_file)
+        translated = read_minute(path_to_translated_file)
+
+        model_name = language_2_para_model_mappings[language]
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        model = MBartForConditionalGeneration.from_pretrained(model_name)
+
+        pred_df = pd.DataFrame({
+            'input_text': minutes,
+            'target_text': translated
+        })
+
+        # check if mt_prediction -> input length ratio is normal
+        pred_df["is_normal"] = pred_df.progress_apply(
+            lambda row: check_for_verbosity(row['input_text'], row['target_text']),
+            axis=1
+        )
+        not_normal_seq_index = pred_df.index[pred_df['is_normal'] == True].to_list()
+        columns = ["input_text", "target_text"]
+        pred_normal_df = pred_df[~pred_df.index.isin(not_normal_seq_index)][columns]
+        pred_not_normal_df = pred_df[pred_df.index.isin(not_normal_seq_index)][columns]
+
+        # apply paraphrase prompt 
+        pred_not_normal_df["target_text"] = pred_not_normal_df.progress_apply(
+            lambda row: append_paraphrase_prompt(row['input_text'], row['target_text']),
+            axis=1
+        )
+
+        # temp folder to store files
+        path_to_temp_folder = os.path.join(current_directory, f"temp_{process_code}")
+        if not os.path.exists(path_to_temp_folder):
+            os.makedirs(path_to_temp_folder)
+
+
+        path_to_not_normal_file = os.path.join(path_to_temp_folder, "test_not_normal.csv")
+        path_to_normal_file = os.path.join(path_to_temp_folder, "test_normal.csv")
+        pred_not_normal_df.to_csv(path_to_not_normal_file, index=False)
+        pred_normal_df.to_csv(path_to_normal_file, index=False)
+
+        processed_raw_test_dataset = load_dataset('csv', data_files={"test": path_to_not_normal_file})
+        test_dataloader = DataLoader(processed_raw_test_dataset["test"], batch_size=1, num_workers=0)
+
+        predictions = []
+        for batch in tqdm(test_dataloader):
+            translated = model.generate(**tokenizer(batch['target_text'], return_tensors="pt", padding=True))
+            predictions.extend([tokenizer.decode(t, skip_special_tokens=True) for t in translated])
+
+        pred_not_normal_df["target_text"] = predictions
+        processed_pred_df = pd.concat([pred_normal_df, pred_not_normal_df]).sort_index()
+        shutil.rmtree(path_to_temp_folder)
+
+        processed_translated = processed_pred_df["target_text"].to_list()
+        save_2_text(processed_translated, path_to_translated_file)
+
+
+def generate_translated_document(languages, process_code):
+    # generate translation
+    process_translation(languages=languages, process_code=process_code)  
+    # rephrase translation for isometric   
+    process_paraphrase(languages=languages, process_code=process_code)
